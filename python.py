5a97b4176254d8c7cd444fe4d0664b8d995ac4fb
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 18:40:03 +0400

    for loop, initialisation, add items in list

--- python.py
+++ python.py
@@ -19,14 +19,19 @@
     ### Loops ###
 
 # while loop
-n = 100
-i = 0
-while i < n:
-    print(i)
-    i = i+1
+# n = 100
+# i = 0
+# while i < n:
+#     print(i)
+#     i = i+1
 
 # For loop
-number = [0,1,2,3,4,5,6,7,8,9]
-for i in number:
-    print(i**2)
+# number = [0,1,2,3,4,5,6,7,8,9]
+# for i in number:
+#     print(i**2)
+n=100
+numbers = [] #initialisation
+for i in range(100):
+    numbers.append(i**2)
+    print(numbers)
 
