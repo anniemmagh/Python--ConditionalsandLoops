d104a448a10c0b2ca619b7388851b3b89657e48d
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 18:01:25 +0400

    elif statement

--- python.py
+++ python.py
@@ -1,7 +1,17 @@
 #conditionals
 #IF, else, elif
+#if
 n = 10
 if n % 2 == 0:
     print("This number is even") 
 else:
     print("This number is odd")
+    
+    #elif
+    n = 13
+if n > 0:
+    print("This number is Positive") 
+elif n<0:
+    print("This number is negative")    
+else:
+    print("This number is Zero")
