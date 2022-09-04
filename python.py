c2d6752dc98d2ed3fb105fffaab733fa0570c10d
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 18:08:50 +0400

    while loop

--- python.py
+++ python.py
@@ -1,17 +1,27 @@
 #conditionals
 #IF, else, elif
 #if
-n = 10
-if n % 2 == 0:
-    print("This number is even") 
-else:
-    print("This number is odd")
+# n = 10
+# if n % 2 == 0:
+#     print("This number is even") 
+# else:
+#     print("This number is odd")
     
-    #elif
-    n = 13
-if n > 0:
-    print("This number is Positive") 
-elif n<0:
-    print("This number is negative")    
-else:
-    print("This number is Zero")
+#     #elif
+#     n = 13
+# if n > 0:
+#     print("This number is Positive") 
+# elif n<0:
+#     print("This number is negative")    
+# else:
+#     print("This number is Zero")
+
+    ### Loops ###
+
+# while loop
+n = 100
+i = 0
+while i < n:
+    print(i)
+    i = i+1
+
